62cf2b63845f19b41e1eaeb6c3428f7e4643a978
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-10-27 19:56:18 +0300

    features: add adaptive + same js

--- pages/script.js
+++ pages/script.js
@@ -10,6 +10,8 @@ const profileTitle = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__job');
 const form = document.querySelector('.popup__form');
 
+// const popupBtn = document.querySelector('.popup__btn');
+
 profileBtnEdit.addEventListener("click", function () {
 	popupInputTitle.value = profileTitle.innerText;
 	popupInputSubtitle.value = profileSubtitle.innerText;
@@ -19,8 +21,14 @@ popupClose.addEventListener("click", function () {
 	popup.classList.toggle('popup_opened');
 });
 
-form.onsubmit = function () {
-	console.log('form.onsubmit');
+// popupBtn.addEventListener("click", function () {
+// 	console.log('1-click!!!!');
+// });
+
+form.onsubmit = function (e) {
+	e.preventDefault();// Эта строчка отменяет стандартные действия при .onsubmit
+	// Находим сам попап
+	let popupElement = document.querySelector('.popup');
 	// Находим форму в DOM
 	let formElement = document.querySelector('.popup__form');
 	// Находим поля формы в DOM
@@ -29,26 +37,31 @@ form.onsubmit = function () {
 
 	// Обработчик «отправки» формы, хотя пока
 	// она никуда отправляться не будет
-	function formSubmitHandler(evt) {
-		console.log('formSubmitHandler');
+	function formSubmitHandler(evt) { console.log('formSubmitHandler(evt)!!!!');
 		evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 		// Так мы можем определить свою логику отправки.
 		// О том, как это делать, расскажем позже.
 
 		// Получите значение полей jobInput и nameInput из свойства value
-		console.log('jobInput.value = ', jobInput.value);
-		console.log('nameInput.value = ', nameInput.value);
+		let nameInputNew = nameInput.value;
+		let jobInputNew = jobInput.value;
 
 		// Выберите элементы, куда должны быть вставлены значения полей
-		const profileName = document.querySelector('.profile__name');
-		const profileJob = document.querySelector('.profile__job');
+		let profileName = document.querySelector('.profile__name');
+		let profileJob = document.querySelector('.profile__job');
 
 		// Вставьте новые значения с помощью textContent
-		profileName.value = nameInput.value;
-		profileJob.value = jobInput.value;
+		profileName.textContent = nameInputNew;
+		profileJob.textContent = jobInputNew;
+
+		//закрыть popup
+		console.log('popupElement = ',popupElement);
+		console.log('popupElement.classList = ',popupElement.classList);
+		popupElement.classList.remove('popup_opened');
 	}
 
 	// Прикрепляем обработчик к форме:
 	// он будет следить за событием “submit” - «отправка»
+	// formElement.addEventListener('click', formSubmitHandler);
 	formElement.addEventListener('submit', formSubmitHandler);
 };
