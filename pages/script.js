c232b46bc732c8b7228e31f5984681238732ebf1
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-10-28 22:58:34 +0300

    Edits: for button hover, favicon, head active hover disabled imgs

--- pages/script.js
+++ pages/script.js
@@ -3,23 +3,6 @@
 let profileBtnEdit = document.querySelector('.profile__btn_edit');
 let popupClose = document.querySelector('.popup__close');
 
-function openPopup() {
-    const nameInput = document.querySelector('.popup__input_title');
-    const jobInput = document.querySelector('.popup__input_subtitle');
-    const popupElement = document.querySelector('.popup');
-    const profileTitle = document.querySelector('.profile__name');
-    const profileSubtitle = document.querySelector('.profile__job');
-
-    nameInput.value = profileTitle.innerText;
-    jobInput.value = profileSubtitle.innerText;
-    popupElement.classList.toggle('popup_opened');
-};
-
-function closePopup() {
-    const popup = document.querySelector('.popup');
-
-    popup.classList.toggle('popup_opened');
-};
 // Находим сам попап
 let popupElement = document.querySelector('.popup');
 // Находим форму в DOM
@@ -28,29 +11,37 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_title');
 let jobInput = document.querySelector('.popup__input_subtitle');
 
+function openPopup() {
+    const nameInput = document.querySelector('.popup__input_title');
+    const jobInput = document.querySelector('.popup__input_subtitle');
+    const profileTitle = document.querySelector('.profile__name');
+    const profileSubtitle = document.querySelector('.profile__job');
+    nameInput.value = profileTitle.innerText;
+    jobInput.value = profileSubtitle.innerText;
+    popupElement.classList.add('popup_opened');
+};
+
+function closePopup() {
+    const popupElement = document.querySelector('.popup');
+    popupElement.classList.remove('popup_opened');
+};
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+    // Получаем значение полей jobInput и nameInput из свойства value
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
+    // Выбераем элементы, куда должны быть вставлены значения полей
     let profileName = document.querySelector('.profile__name');
     let profileJob = document.querySelector('.profile__job');
-
-    // Вставьте новые значения с помощью textContent
+    // Вставляем новые значения с помощью textContent
     profileName.textContent = nameInputValue;
     profileJob.textContent = jobInputValue;
-
-    //закрыть popup
+    //закрываем popup
     popupElement.classList.remove('popup_opened');
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
