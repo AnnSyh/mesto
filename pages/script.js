d1d0302616429c1b848d5d7d23997d25eae63e06
Аня <suharevskaya@firecode.ru>
2021-11-09 17:41:26 +0300

    add heartClick() and some edits

--- pages/script.js
+++ pages/script.js
@@ -27,6 +27,7 @@ const initialCards = [
 ];
 
 
+
 //находим список в кот надо встаивть карточки
 const cardsList = document.querySelector('.cards__list');
 //находим кнопки кот вызывают всплытие/закрытие окна редактирования
@@ -37,7 +38,7 @@ let profileBtnAdd = document.querySelector('.profile__btn_user_add');
 let popupCloseEditProfile = document.querySelector('.popup__close_edit_profile');
 let popupCloseAddPlace = document.querySelector('.popup__close_add_plaсe');
 // Находим сам попап
-let popupElement = document.querySelector('.popup_edit_profile');
+// let popupElement = document.querySelector('.popup_edit_profile');
 let popupEditProfile = document.querySelector('.popup_edit_profile');
 let popupAddPlaceElement = document.querySelector('.popup_add_plaсe');
 // Находим форму в DOM
@@ -52,64 +53,36 @@ let placeImgInput = document.querySelector('.popup__input_plaсe_img');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
+
 function addCards(data) {
     for (let i = 0; i < data.length; i++) {
-
-        const cardsLi = document.createElement('li');
-        cardsLi.classList.add('cards__item');
-
-        const cardsImg = document.createElement('img');
-        cardsImg.classList.add('cards__img');
-        cardsImg.src = data[i].link;
-
-        const cardsText = document.createElement('div');
-        cardsText.classList.add('cards__text');
-
-        const cardsTitle = document.createElement('h2');
-        cardsTitle.classList.add('cards__title');
-        cardsTitle.textContent = data[i].name;
-
-        const likeButton = document.createElement('button');
-        likeButton.classList.add('cards__heart');
-
-        cardsText.append( cardsTitle, likeButton);
-        cardsLi.append( cardsImg, cardsText);
-
-        cardsList.append(cardsLi);
+        addCard(data[i].name, data[i].link)
     }
 }
 
-function addCard(name,src) {
-
-        const cardsLi = document.createElement('li');
-        cardsLi.classList.add('cards__item');
-
-        const cardsImg = document.createElement('img');
-        cardsImg.classList.add('cards__img');
-        cardsImg.src = src;
-        
-        const cardsText = document.createElement('div');
-        cardsText.classList.add('cards__text');
-
-        const cardsTitle = document.createElement('h2');
-        cardsTitle.classList.add('cards__title');
-        cardsTitle.textContent = name;
-
-        const likeButton = document.createElement('button');
-        likeButton.classList.add('cards__heart');
-
-        cardsText.append( cardsTitle, likeButton);
-        cardsLi.append( cardsImg, cardsText);
-
-        cardsList.prepend(cardsLi);
-   
+function addCard(name, src) {
+    const cardsLi = document.createElement('li');
+    cardsLi.classList.add('cards__item');
+    const cardsImg = document.createElement('img');
+    cardsImg.classList.add('cards__img');
+    cardsImg.src = src;
+    const cardsText = document.createElement('div');
+    cardsText.classList.add('cards__text');
+    const cardsTitle = document.createElement('h2');
+    cardsTitle.classList.add('cards__title');
+    cardsTitle.classList.add('text-overflow');
+    cardsTitle.textContent = name;
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('cards__heart');
+    cardsText.append(cardsTitle, likeButton);
+    cardsLi.append(cardsImg, cardsText);
+    cardsList.prepend(cardsLi);
 }
 
 function openPopup() {
     nameInput.value = profileName.innerText;
     jobInput.value = profileJob.innerText;
-    popupElement.classList.add('popup_opened');
-    // popupAddPlaceElement.classList.add('popup_opened');
+    popupEditProfile.classList.add('popup_opened');
 };
 
 function openPopupAdd() {
@@ -117,13 +90,13 @@ function openPopupAdd() {
 };
 
 function closePopup() {
-    popupElement.classList.remove('popup_opened');
+    popupEditProfile.classList.remove('popup_opened');
     popupAddPlaceElement.classList.remove('popup_opened');
 };
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler(evt) { console.log('formSubmit 1111');
+function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Получаем значение полей jobInput и nameInput из свойства value
     let nameInputValue = nameInput.value;
@@ -136,25 +109,26 @@ function formSubmitHandler(evt) { console.log('formSubmit 1111');
     // popupElement.classList.remove('popup_opened');
     closePopup();
 }
-function formAddPlaceSubmitHandler(evt) { console.log('formSubmit 22222');
+function formAddPlaceSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Получаем значение полей jobInput и nameInput из свойства value
     let placeNameInputValue = placeNameInput.value;
     let placeImgInputValue = placeImgInput.value;
-    // Выбераем элементы, куда должны быть вставлены значения полей
-    // Вставляем новые значения с помощью textContent
-    console.log('placeNameInputValue = ', placeNameInputValue);
-    console.log('placeImgInputValue = ', placeImgInputValue);
-
-
+    // Предаем их в создаваймую карточку
     addCard(placeNameInputValue, placeImgInputValue);
-
     //закрываем popup
     closePopup();
 }
 
-console.log('formElement = ',formElement);
-console.log('formAddPlaceElement = ',formAddPlaceElement);
+function heartClick() {
+    //находим сердечки
+    let cardsHeart = document.querySelectorAll('.cards__heart');
+    for (let i = 0; i < cardsHeart.length; i++) {
+        cardsHeart[i].addEventListener('click', function () {
+            cardsHeart[i].classList.toggle('cards__heart_active');
+        });
+    }
+};
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
@@ -168,3 +142,8 @@ profileBtnAdd.addEventListener('click', openPopupAdd);
 popupCloseAddPlace.addEventListener('click', closePopup);
 
 addCards(initialCards);
+
+heartClick();
+
+
+
