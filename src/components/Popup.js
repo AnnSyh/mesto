8ba6ab7a89b36b47cab3d4af82775a22a66f5936
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2022-01-29 23:10:33 +0300

    clean code

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,23 +6,23 @@ export class Popup {
     this._close = this._popup.querySelector(".popup__close");
     this._overlay = this._popup.querySelector(".popup__overlay");
     this._submit = this._popup.querySelector(".popup__btn");
-    this._handleEscClose = this._handleEscClose.bind(this); // <==== привязали 1 раз ===
+    this._handleEscClose = this._handleEscClose.bind(this); // <==== привязала 1 раз ===
   }
 
   openPopup() {
     this._popup.classList.add("popup_opened");
     this.setEventListeners();
-    document.addEventListener("keydown", this._handleEscClose); // <=== только cсылку передаете ==
-  }
-  setEventListeners() {
-    this._close.addEventListener("click", (evt) => this.closePopup(evt));
-    this._overlay.addEventListener("click", (evt) => this.closePopup(evt));
-    // this._handleEscClose();
+    document.addEventListener("keydown", this._handleEscClose); // <=== только cсылку передаю ==
   }
 
   closePopup() {
     this._popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose); // <==== такая же ссылка должна быть
+  }
+
+  setEventListeners() {
+    this._close.addEventListener("click", (evt) => this.closePopup(evt));
+    this._overlay.addEventListener("click", (evt) => this.closePopup(evt));
   }
 
   _handleEscClose(evt) {
