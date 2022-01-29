4f6830a4a953a6dc9e56019f01333af03d133648
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2022-01-29 23:16:02 +0300

    clean code popup files

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,35 +1,34 @@
-'use strict'
-import { Popup } from './Popup.js';
+"use strict";
+import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
+  constructor(popup, handleFormSubmit) {
+    super(popup);
+    this._form = this._popup.querySelector(".form");
+    this._inputList = this._form.querySelectorAll(".popup__input");
+    this._handleFormSubmit = handleFormSubmit;
+  }
 
-    constructor(popup, handleFormSubmit) {
-        super(popup);
-        this._form = this._popup.querySelector('.form');
-        this._inputList = this._form.querySelectorAll('.popup__input');
-        this._handleFormSubmit = handleFormSubmit;
-    }
+  _getInputValues() {
+    this._formValues = {};
+    this._inputList.forEach((element) => {
+      this._formValues[element.name] = element.value;
+    });
+    return this._formValues;
+  }
 
-    _getInputValues() {
-        this._formValues = {};
-        this._inputList.forEach(element => {
-            this._formValues[element.name] = element.value;
-        });
-        return this._formValues;
-    }
+  _handler = (evt) => {
+    this._handleFormSubmit(evt, this._getInputValues());
+    this._closePopup();
+  };
 
-    _handler = (evt) => {
-        this._handleFormSubmit(evt, this._getInputValues());
-        this._closePopup();
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", this._handler);
+  }
 
-    setEventListeners() {
-        super.setEventListeners();
-        this._form.addEventListener('submit', this._handler);
-    }
-
-    _closePopup() {
-        super.closePopup();
-        this._form.reset();
-    }
+  _closePopup() {
+    super.closePopup();
+    this._form.reset();
+  }
 }
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,27 +1,19 @@
-'use strict'
-import { Popup } from './Popup.js';
-
+"use strict";
+import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
+  constructor(popup, text, link) {
+    super(popup);
+    this._popup = popup;
+    this._img = this._popup.querySelector(".popup__img");
+    this._title = this._popup.querySelector(".popup__caption");
+    this._text = text;
+    this._link = link;
+  }
 
-    constructor(popup, text, link) {
-        super(popup);
-        this._popup = popup;
-        this._img = this._popup.querySelector('.popup__img');
-        this._title = this._popup.querySelector('.popup__caption');
-
-        // debugger;
-
-        this._text = text;
-        this._link = link;
-    }
-
-    openPopup(text, link) {
-        super.openPopup();
-        // устанавливаем ссылку
-        // устанавливаем подпись картинке
-        //открываем попап универсальной функцией, которая навешивает обработчик Escape внутри себя
-        this._title.innerText = text;
-        this._img.alt = text;
-        this._img.src = link;
-    }
+  openPopup(text, link) {
+    super.openPopup();
+    this._title.innerText = text; // устанавливаем подпись картинке
+    this._img.alt = text; // устанавливаем alt картинке
+    this._img.src = link; // устанавливаем ссылку
+  }
 }
